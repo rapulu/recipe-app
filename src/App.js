644085541a2f209e77ff43eddda64aca209afb8a
Paul Rapulu <rapulu@yahoo.com>
2019-04-05 08:11:38 +0100

    Fix: prettier

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React, { Component } from 'react';
-import Form from './Form'
-import './App.css';
-import { async } from 'q';
+import React, { Component } from "react";
+import Form from "./Form";
+import "./App.css";
+import { async } from "q";
 
 const API_KEY = "7c11c672aaf4adac74fdf82c190dbb14";
 
 class App extends Component {
-
   state = {
     recipes: []
-  }
+  };
 
-  getRecipe = async (e) => {
+  getRecipe = async e => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
-    const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=5`);
-    
+    const api_call = await fetch(
+      `https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=5`
+    );
+
     const data = await api_call.json();
 
-    this.setState({ recipes: data.recipes});
+    this.setState({ recipes: data.recipes });
 
-    console.log(this.state.recipes)
-  }
+    console.log(this.state.recipes);
+  };
 
   render() {
     return (
@@ -29,11 +30,11 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
-        <Form getRecipe={this.getRecipe}/>
+        <Form getRecipe={this.getRecipe} />
 
-        { this.state.recipes.map((recipe) => {
-          return <p key={recipe.recipe_id}>{ recipe.title }</p>
-        }) }
+        {this.state.recipes.map(recipe => {
+          return <p key={recipe.recipe_id}>{recipe.title}</p>;
+        })}
       </div>
     );
   }
