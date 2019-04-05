b12cba66da26c1d4cbe436b6d59d10536889ce62
Paul Rapulu <rapulu@yahoo.com>
2019-04-05 01:59:16 +0100

    added: search form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Form from './Form'
 import './App.css';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
+        <Form />
       </div>
     );
   }
---
+++ src/Form.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+let Form = () => (
+  <div>
+    <input type="text" />
+    <button>Search</button>
+  </div>
+)
+
+export default Form;
